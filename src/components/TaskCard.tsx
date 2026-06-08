bebdf196a0590b578f554c433ed6c1fdea95e5f3
vadimk321 <8246vadim8246@mail.ru>
2026-06-08 16:33:13 +0300

    Add styles for TaskCard

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,12 +1,18 @@
 import { useState, useEffect } from 'react';
 import type { Task, MoveSide } from '../types/task';
 
-import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import CheckIcon from '@mui/icons-material/Check';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 
 type TaskCardProps = {
@@ -61,66 +67,93 @@ function TaskCard(props: TaskCardProps ) {
   }
 
   return (
-    <div className="task-card-column">
-      {editingId !== task.id ? 
-      <div>
-        {task.status !== 'todo' 
-        && 
-        <Button 
-          onClick={(e) => handleMoveTask(e, task.id, 'back')}
-          variant="contained"
-          size="small"
-          >
-            <ArrowBackIcon/>
-        </Button>}
-        <span>{task.title}</span>
-        {task.status !== 'done' 
-        && 
-        <Button 
-          onClick={(e) => handleMoveTask(e, task.id, 'forward')}
-          variant="contained"
-          size="small"
-          >
-            <ArrowForwardIcon/>
-        </Button>}
-        <Button 
-          onClick={handleDeleteTask}
-          variant="outlined"
-          color="error"
-          size="small"
-          >
-          <DeleteIcon/>
-        </Button>
-        <Button
-          onClick={() => startEdit(task.id)}
-          variant="outlined"
-          size="small"
-        >
-          <EditIcon/>
-        </Button>  
-      </div> : 
-      <div>
-        <input 
-          value={editingInput} 
-          onChange={(e) => setEditingInput(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              handleSaveEdit();
-            } else if (e.key === 'Escape') {
-              handleCancelTask();
-            }
-          }}
-          />
-        <Button 
-          onClick={handleSaveEdit}
-          variant="contained"
-          color="success"
-          size="small"
-          >
-          <CheckIcon/>
-        </Button>
-      </div>}
-    </div>
+    <Card sx={{ 
+      borderRadius: 5,
+      cursor: 'pointer',
+      display: 'flex',
+      flexDirection: 'column',
+      mb: 2,
+      p: 2,
+      maxWidth: '250px'
+      }}>
+      
+        {editingId !== task.id ? 
+        <Box>
+
+          <CardContent>
+            <Typography sx={{
+              wordBreak: 'break-word',
+              overflowWrap: 'anywhere'
+            }}>
+              {task.title}
+            </Typography>
+          </CardContent>
+
+          <CardActions sx={{
+              mx: 'auto',
+              gap: 1,
+            }}>
+            {task.status !== 'todo' 
+            && 
+            <IconButton 
+              onClick={(e) => handleMoveTask(e, task.id, 'back')}
+              size="small"
+              >
+              <ArrowBackIcon/>
+            </IconButton>}
+          
+            <IconButton 
+              onClick={handleDeleteTask}
+              color="error"
+              size="small"
+              >
+              <DeleteIcon/>
+            </IconButton>
+
+            <IconButton
+              onClick={() => startEdit(task.id)}
+              size="small"
+            >
+              <EditIcon/>
+            </IconButton>
+
+            {task.status !== 'done' 
+            && 
+            <IconButton 
+              onClick={(e) => handleMoveTask(e, task.id, 'forward')}
+              size="small"
+              >
+              <ArrowForwardIcon/>
+            </IconButton>}
+
+          </CardActions>
+        </Box> : 
+        <Box>
+          <TextField
+            sx={{
+              mb: 2
+            }}
+            fullWidth
+            size="small"
+            value={editingInput} 
+            onChange={(e) => setEditingInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleSaveEdit();
+              } else if (e.key === 'Escape') {
+                handleCancelTask();
+              }
+            }}
+            />
+          <IconButton 
+            onClick={handleSaveEdit}
+            color="success"
+            size="small"
+            >
+            <CheckIcon/>
+          </IconButton>
+        </Box>}
+    </Card>
   )
 }
 
